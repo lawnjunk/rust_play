225af1f2f2e78b4e1356a89d773ba4d6c594f5a0
Duncan Marsh <slugbyte@slugbyte.com>
2022-01-29 12:55:46 +0800

    [notch] better parsing

--- toy/get_char/src/main.rs
+++ toy/get_char/src/main.rs
@@ -16,22 +16,63 @@ struct CharSource {
     handle: Rc::<JoinHandle<()>>,
 }
 
+#[derive(Debug,Clone,Copy)]
+enum KeyValue {
+    Letter(char),
+    Esc,
+    Tab,
+    Enter,
+    Backspace,
+    Del,
+    Insert,
+    Up,
+    Down,
+    Left,
+    Right,
+    PageUp,
+    PageDown,
+    Home,
+    End,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+}
+
+#[derive(Debug)]
+struct Key {
+    value: KeyValue,
+    special: Option<KeyValue>,
+    ctrl: bool,
+    alt: bool,
+}
+
 impl CharSource {
     fn new() -> CharSource {
         let (tx, rx) = mpsc::channel::<u8>();
 
         let handle = thread::spawn(move || {
-            println!("hahah ");
             let stdin = io::stdin();
             let mut stdin = stdin.lock();
-            let mut local_buf = [0;100];
+            let mut local_buf = [0;10];
 
             loop {
                 if let Ok(read_count) =  stdin.read(&mut local_buf) {
+                    write_code("0G");
                     println!("\nread_count: {}", read_count);
                     for i in 0..read_count {
                         tx.send(local_buf[i]).unwrap();
                     }
+                    write_code(&format!("0G{:?}", parse_key(&mut local_buf[..read_count], read_count)));
+                    println!();
                 }
             }
         });
@@ -61,6 +102,203 @@ fn write_code(code: &str) {
     print!("\x1b[{}", code);
 }
 
+fn pares_key_byte(byte: u8, alt: bool) -> Option<Key> {
+    if byte == 8 {
+        return Some(Key {
+            value: KeyValue::Letter((byte + 96) as char),
+            special: Some(KeyValue::Backspace),
+            ctrl: true,
+            alt,
+        })
+    }
+    if byte == 13 {
+        return Some(Key {
+            value: KeyValue::Letter((byte + 96) as char),
+            special: Some(KeyValue::Enter),
+            ctrl: true,
+            alt,
+        })
+    }
+
+    if byte == 9 {
+        return Some(Key {
+            value: KeyValue::Letter((byte + 96) as char),
+            special: Some(KeyValue::Tab),
+            ctrl: true,
+            alt,
+        })
+    }
+
+    // CTRL + CHAR
+    if byte < 27 {
+        return Some(Key {
+            value: KeyValue::Letter((byte + 96) as char),
+            special: None,
+            ctrl: true,
+            alt,
+        })
+    }
+
+    // ESCAPE
+    if byte == 27 {
+        return Some(Key {
+            value: KeyValue::Esc,
+            special: None,
+            ctrl: false,
+            alt,
+        })
+    }
+
+    // BACKSPACE 
+    if byte == 127 {
+        return Some(Key {
+            value: KeyValue::Backspace,
+            special: None,
+            ctrl: false,
+            alt,
+        })
+    }
+
+    if byte > 31 {
+        return Some(Key {
+            value: KeyValue::Letter(byte as char),
+            special: None,
+            ctrl: false,
+            alt,
+        })
+    }
+    None
+}
+
+fn parse_key(buf: &mut [u8], count: usize) -> Vec<Option<Key>> {
+    write_code("0G");
+    println!("buf: {:?}", buf);
+    let mut result: Vec<Option<Key>> = vec![];
+
+    if count == 0  {
+        return result
+    }
+
+    if count == 1 {
+        result.push(pares_key_byte(buf[0], false));
+    }
+
+    if count == 2 {
+        if buf[0] == 27 {
+            result.push(pares_key_byte(buf[1], true));
+        } else {
+            result.push(pares_key_byte(buf[0], false));
+            result.push(pares_key_byte(buf[1], false));
+        }
+    }
+
+
+    let create_page_key = | result: &mut Vec<_>, buf: &[_], starts_with: &[_], value: KeyValue | {
+        if buf.ends_with(&[starts_with, &[126]].concat()) {
+            result.push(Some(Key {
+                value: value,
+                special: None,
+                ctrl: false,
+                alt: false,
+            }))
+        }
+
+        if buf.starts_with(starts_with) && buf.ends_with(&[59, 53, 126]) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: true,
+                alt: false,
+            }))
+        }
+
+        if buf.starts_with(starts_with) && buf.ends_with(&[59, 51, 126]) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: false,
+                alt: true,
+            }))
+        }
+
+        if buf.starts_with(starts_with) && buf.ends_with(&[59, 55, 126]) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: true,
+                alt: true,
+            }))
+        }
+    };
+
+    let create_nav_key = | result: &mut Vec<_>, buf: &[_], start: &[_], end: u8, value: KeyValue | {
+        if buf.ends_with(&[start, &[end]].concat()) {
+            result.push(Some(Key {
+                value: value,
+                special: None,
+                ctrl: false,
+                alt: false,
+            }))
+        }
+
+        if buf.starts_with(&[start, &[49, 59, 53], &[end]].concat()) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: true,
+                alt: false,
+            }))
+        }
+
+        if buf.starts_with(&[start, &[49, 59, 51], &[end]].concat()) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: false,
+                alt: true,
+            }))
+        }
+
+        if buf.starts_with(&[start, &[49, 59, 55], &[end]].concat()) {
+            result.push(Some(Key {
+                value,
+                special: None,
+                ctrl: true,
+                alt: true,
+            }))
+        }
+    };
+
+    // more than two byte
+    if buf[0] == 27 {
+        // page up 
+        let page_up: [u8;3] = [27, 91, 53];
+
+
+        create_page_key(&mut result, &buf, &[27, 91, 53], KeyValue::PageUp);
+        create_page_key(&mut result, &buf, &[27, 91, 54], KeyValue::PageDown);
+
+        create_page_key(&mut result, &buf, &[27, 91, 50], KeyValue::Insert);
+        create_page_key(&mut result, &buf, &[27, 91, 51], KeyValue::Del);
+
+        create_nav_key(&mut result, &buf, &[27, 79], 80, KeyValue::F1);
+        create_nav_key(&mut result, &buf, &[27, 79], 81, KeyValue::F2);
+        create_nav_key(&mut result, &buf, &[27, 79], 82, KeyValue::F3);
+
+        create_nav_key(&mut result, &buf, &[27, 79], 82, KeyValue::F3);
+
+        create_nav_key(&mut result, &buf, &[27, 91], 70, KeyValue::End);
+        create_nav_key(&mut result, &buf, &[27, 91], 72, KeyValue::Home);
+        create_nav_key(&mut result, &buf, &[27, 91], 65, KeyValue::Up);
+        create_nav_key(&mut result, &buf, &[27, 91], 66, KeyValue::Down);
+        create_nav_key(&mut result, &buf, &[27, 91], 67, KeyValue::Right);
+        create_nav_key(&mut result, &buf, &[27, 91], 68, KeyValue::Left);
+    }
+
+    buf.fill(0);
+    return result
+}
+
 fn main() {
     let tty = File::open("/dev/tty").unwrap();
     let mut guard = TtyModeGuard::new(tty.as_raw_fd()).unwrap();
@@ -69,11 +307,6 @@ fn main() {
         let mut cs = CharSource::new();
         loop {
             if let Some(recent) = cs.read() {
-                write_code("0G");
-                println!("code: {:?}", recent);
-                if recent == b'\x1b' {
-                    println!("wwwwww");
-                }
                 if recent == 113  || recent == 3 {
                     break;
                 }
